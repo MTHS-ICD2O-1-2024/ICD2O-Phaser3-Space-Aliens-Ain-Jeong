623f5e0ee3096185879baa54f9ce22497c063c17
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-04-11 17:13:02 +0000

    init

---
+++ js/game.js
@@ -0,0 +1,32 @@
+/*global Phaser */
+
+// Copyright (c) 2025 Ain Jeong All rights reserved
+//
+// Created by: Ain Jeong
+// Created on: Apr 2025
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
